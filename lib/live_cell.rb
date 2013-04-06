6c6b95ac0a1747bd0e9e65316a17e06ddf4760e9
Steven Nunez <snunez@cyrusinnovation.com>
2013-04-06 15:01:04 -0400

    Add first rule of GOF. Less than 2 alive, and Live Cells die

--- lib/live_cell.rb
+++ lib/live_cell.rb
@@ -1,3 +1,13 @@
 class LiveCell
+  def forecast(neighbors:0)
+    if neighbors >= 2
+      LiveCell
+    else
+      DeadCell
+    end
+  end
+end
 
+# Created but has not functionality. Just put here since I need it
+class DeadCell
 end
--- spec/live_cell_spec.rb
+++ spec/live_cell_spec.rb
@@ -2,4 +2,10 @@ require 'minitest/autorun'
 require_relative '../lib/live_cell'
 
 describe LiveCell do 
+  it "dies with fewer than 2 live neighbors" do
+    lc = LiveCell.new
+    lc.forecast(neighbors: 0).must_equal(DeadCell)
+    lc.forecast(neighbors: 1).must_equal(DeadCell)
+    lc.forecast(neighbors: 2).must_equal(LiveCell)
+  end
 end
