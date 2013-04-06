0b1ca89f67e2cd8fb3cdf04723450581431b08cf
Steven Nunez <snunez@cyrusinnovation.com>
2013-04-06 19:01:58 -0400

    Write runner script

--- game_of_life.rb
+++ game_of_life.rb
@@ -5,3 +5,17 @@
 # Any live cell with three live neighbours dies, as if by overcrowding. 
 # Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction. 
 # Neighbours are any two triangles that shares a side. 
+require_relative 'lib/grid'
+
+positions = 20.times.map do 
+  rand(2)
+end
+puts positions.class
+grid = Grid.new(*positions)
+generation_count = 1
+1.upto(10) do |gen|
+  puts "Generation ##{gen}"
+  grid.raw_positions.each {|rp| print "#{rp} "}
+  puts
+  grid.next_generation! 
+end
